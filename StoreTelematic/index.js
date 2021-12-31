fbc6d5f495efbb3616f74ea6dcb5d042364c8558
Bo <bo.robbrecht@student.howest.be>
2021-12-31 11:08:59 +0100

    Able to check & return error code if body is not array

--- StoreTelematic/index.js
+++ StoreTelematic/index.js
@@ -1,21 +1,20 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('HTTP function "StoreTelemetric" triggered!');
+    const neededKeys = ['roll', 'pitch', 'lat', 'long', 'alt', 'date', 'time']
+    try {
+        const telemetric = req.body;
+        if (!Array.isArray(telemetric)) { throw generateError(400, "Bad request: The given body is not an array")}
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    if (name) {
-        context.bindings.outputDocument = JSON.stringify({
-            // create a random ID
-            id: new Date().toISOString() + Math.random().toString().substr(2, 8),
-            name: name
-        });
+    } catch(err) { 
+        context.res = { 
+            status: err.code,
+            body: err.message
+        }
     }
-    
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+}
+
+function generateError(statusCode = 500, string = "Internal Server Error") { 
+    let err = new Error(string)
+    err.code = statusCode
+    return err
 }
