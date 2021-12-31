422a9d14d21b6174739dd263430c216c5fd47588
Bo <bo.robbrecht@student.howest.be>
2021-12-31 11:59:07 +0100

    Added Key check

--- StoreTelematic/index.js
+++ StoreTelematic/index.js
@@ -3,18 +3,56 @@ module.exports = async function (context, req) {
     const neededKeys = ['roll', 'pitch', 'lat', 'long', 'alt', 'date', 'time']
     try {
         const telemetric = req.body;
-        if (!Array.isArray(telemetric)) { throw generateError(400, "Bad request: The given body is not an array")}
+        arrayIntegrity(telemetric)
+        keyIntegrity(telemetric, neededKeys)
 
-    } catch(err) { 
-        context.res = { 
+    } catch (err) {
+        context.res = {
             status: err.code,
-            body: err.message
+            body: {
+                code: err.code,
+                keyword: err.keyword,
+                message: err.message,
+                bag: (err.bag ? err.bag : null)
+            }
         }
     }
 }
 
-function generateError(statusCode = 500, string = "Internal Server Error") { 
-    let err = new Error(string)
+function generateError(statusCode = 500, keyword= "Internal Server Error", msg = "Internal Server Error", errorbag) {
+    let err = new Error(msg)
     err.code = statusCode
+    err.keyword = keyword
+    if (errorbag) { err.bag = errorbag }
     return err
 }
+
+function arrayIntegrity(target) {
+    if (!Array.isArray(target)) { throw generateError(400, "Bad request", "The given body is not an array") }
+}
+
+function keyIntegrity(target, neededKeys) {
+    let i = 0
+    let errorBag = []
+
+    target.every(entry => {
+        i++
+        neededKeys.forEach(key => {
+            if (!Object.keys(entry).includes(key)) {
+                errorBag.push({ seq: i, key: `${key}` })
+            }
+        })
+    })
+
+    if (errorBag.length > 0) {
+        throw generateError(400, "Bad Request", "Some keys are missing", formatErrorBag(errorBag))
+    }
+
+    function formatErrorBag(errorbag) {
+        let bag = []
+        errorbag.forEach(error => bag.push(`At entry ${error.seq}, the key '${error.key}' is missing`))
+        return bag
+    }
+}
+
+
