9ff9bb7b39aff490c45e69330ac866217ffed3c4
Bo <bo.robbrecht@student.howest.be>
2021-12-31 10:37:01 +0100

    Able to get example function up & running

---
+++ StoreTelematic/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    if (name) {
+        context.bindings.outputDocument = JSON.stringify({
+            // create a random ID
+            id: new Date().toISOString() + Math.random().toString().substr(2, 8),
+            name: name
+        });
+    }
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
