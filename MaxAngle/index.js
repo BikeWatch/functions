705b938930766efc708c67e33c093f2462752021
Bo <bo.robbrecht@student.howest.be>
2022-01-02 19:26:43 +0100

    Updated sample.dat

--- MaxAngle/index.js
+++ MaxAngle/index.js
@@ -2,10 +2,13 @@ const { generateError } = require("../helpers/error");
 const { generateResponse, generateDataResponse } = require("../helpers/response");
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('Trigger "MaxAngle" activated!');
     try {
         inputValidation(req.query)
-        context.res = generateDataResponse(200, context.bindings.inputDocument)
+        let value = (context.bindings.inputDocument[0].pitch > context.bindings.inputDocument[0].roll)
+            ? context.bindings.inputDocument[0].pitch
+            : context.bindings.inputDocument[0].roll
+        context.res = generateDataResponse(200, value)
     } catch(err) { 
         context.res = generateResponse(err.code, err.keyword, err.message, err.bag)
     }
