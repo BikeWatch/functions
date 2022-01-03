f6fe6f462a5c8dbe49020376f4c3f3a333a24c77
Bo <bo.robbrecht@student.howest.be>
2022-01-03 16:17:48 +0100

    Replaced inputValidation references with /helpers/validation.js -> inputValidation(uuid, from, to)

--- MaxAngle/index.js
+++ MaxAngle/index.js
@@ -1,28 +1,15 @@
-const { generateError } = require("../helpers/error");
+const { inputValidation } = require("../helpers/validation");
 const { generateResponse, generateDataResponse } = require("../helpers/response");
 
 module.exports = async function (context, req) {
     context.log('Trigger "MaxAngle" activated!');
     try {
-        inputValidation(req.query)
+        inputValidation(req.query.uuid, req.query.from, req.query.to)
         let value = (context.bindings.inputDocument[0].pitch > context.bindings.inputDocument[0].roll)
             ? context.bindings.inputDocument[0].pitch
             : context.bindings.inputDocument[0].roll
         context.res = generateDataResponse(200, value)
     } catch(err) { 
         context.res = generateResponse(err.code, err.keyword, err.message, err.bag)
-    }
-}
-
-function inputValidation(query) { 
-    if (!query.from || !query.uuid || !query.to) {
-        let errorbag = []
-        (!query.from) ? errorbag.push('The query parameter "from" is missing.') : undefined
-        (!query.uuid) ? errorbag.push('The query parameter "uuid" is missing') : undefined
-        (!query.to) ? errorbag.push('The query parameter "to" is missing') : undefined
-        throw generateError(400, "Bad Request", "One or multiple of the required parameters are missing.", errorbag)
-    }
-    if (Date.parse(query.from) >= Date.parse(query.to)) { 
-        throw generateError(412, "Precondition Failed", "The 'from' date is greater then the 'to' date.", ["This will result resulting in a bug in the space-time paradox. 🌌", "Have fun, space cowboy. 🤠"])
     }
 }
