a9fc5c03882156a09f3fcc5192ff466b703ba30e
Bo <bo.robbrecht@student.howest.be>
2022-01-03 16:44:30 +0100

    Able to calculate distance made between 2 timestamps

--- Distance/index.js
+++ Distance/index.js
@@ -1,8 +1,24 @@
-const { generateResponse } = require("../helpers/response");
+const { generateResponse, generateDataResponse } = require("../helpers/response");
 const { inputValidation } = require("../helpers/validation");
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    inputValidation(req.query.uuid, req.query.from, req.query.to)
-    context.res = generateResponse(200, "OK", "Passed")
+    try {
+        context.log('JavaScript HTTP trigger function processed a request.');
+        inputValidation(req.query.uuid, req.query.from, req.query.to)
+        context.res = generateDataResponse(200, calculateGridDistance(context.bindings.inputDocument))
+    } catch (err) { 
+        context.res = generateResponse(err.code, err.keyword, err.message, err.bag)
+    }
+}
+
+function calculateGridDistance(coordinates) { 
+    let refCoordinate = coordinates.shift()
+    let distance = 0
+    coordinates.map(entry => { 
+        let dx = 71.5 * (refCoordinate.long - entry.long)
+        let dy = 111.3 * (refCoordinate.lat - entry.lat)
+        distance = distance + Math.sqrt(dx * dx + dy * dy)
+        refCoordinate = entry
+    })
+    return Math.round(distance * 1000) / 1000
 }
